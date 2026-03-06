a8c8b252a9b3e0e58607726367f9d5d88f398e5b
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-06 03:32:28 +0300

    VKE-1 fixed FirstScreen structure

--- app/src/main/java/dev/toothlonely/vkeducation/MainActivity.kt
+++ app/src/main/java/dev/toothlonely/vkeducation/MainActivity.kt
@@ -7,13 +7,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.InputTransformation
 import androidx.compose.foundation.text.input.OutputTransformation
@@ -33,6 +37,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -66,67 +71,94 @@ fun FirstScreen(modifier: Modifier = Modifier) {
     val phoneNumber = rememberTextFieldState()
 
     Column(
-        verticalArrangement = Arrangement.Center,
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier.fillMaxSize()
     ) {
-        OutlinedTextField(
-            value = text,
-            onValueChange = {
-                text = it
-            },
-            label = {
-                Text(text = stringResource(R.string.text_for_second_activity))
-            },
-            placeholder = {
-                Text(text = stringResource(R.string.edit_text_placeholder))
-            }
-        )
 
-        Row(
-            horizontalArrangement = Arrangement.SpaceAround,
-            modifier = Modifier.fillMaxWidth()
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .border(
+                    width = 1.dp,
+                    color = Color.Blue,
+                    shape = RoundedCornerShape(5.dp)
+                )
+                .height(250.dp)
         ) {
+            OutlinedTextField(
+                value = text,
+                onValueChange = {
+                    text = it
+                },
+                label = {
+                    Text(text = stringResource(R.string.edit_text_placeholder))
+                }
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Button(
+                    onClick = {
+                        openSecondActivity(context, text)
+                    },
+                    modifier = Modifier.width(150.dp)
+                ) {
+                    Text("Открыть вторую Activity")
+                }
+
+                Button(
+                    onClick = { shareText(context, text) },
+                    modifier = Modifier.width(150.dp)
+                ) {
+                    Text("Поделиться через...")
+                }
+            }
+        }
+
+        Column(
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .border(
+                    width = 1.dp,
+                    color = Color.Blue,
+                    shape = RoundedCornerShape(5.dp)
+                )
+                .height(250.dp)
+                .fillMaxWidth()
+        ) {
+            OutlinedTextField(
+                state = phoneNumber,
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Number
+                ),
+                inputTransformation = InputTransformation.maxLength(10).then {
+                    if (!asCharSequence().isDigitsOnly()) revertAllChanges()
+                },
+                outputTransformation = OutputTransformation {
+                    if (length > 0) insert(0, "(")
+                    if (length > 4) insert(4, ")")
+                    if (length > 8) insert(8, "-")
+                },
+                prefix = { Text("+7") },
+                label = {
+                    Text("Введите номер телефона")
+                }
+            )
+
             Button(
                 onClick = {
-                    openSecondActivity(context, text)
-                },
-                modifier = Modifier.width(150.dp)
+                    openPhoneCall(context, phoneNumber.text as String)
+                }
             ) {
-                Text("Открыть вторую Activity")
-            }
-
-            Button(
-                onClick = { shareText(context, text) },
-                modifier = Modifier.width(150.dp)
-            ) {
-                Text("Поделиться через...")
+                Text("Позвонить другу")
             }
         }
 
-        OutlinedTextField(
-            state = phoneNumber,
-            keyboardOptions = KeyboardOptions(
-                keyboardType = KeyboardType.Number
-            ),
-            inputTransformation = InputTransformation.maxLength(10).then {
-                if (!asCharSequence().isDigitsOnly()) revertAllChanges()
-            },
-            outputTransformation = OutputTransformation {
-                if (length > 0) insert(0, "(")
-                if (length > 4) insert(4, ")")
-                if (length > 8) insert(8, "-")
-            },
-            prefix = { Text("+7") }
-        )
-
-        Button(
-            onClick = {
-                openPhoneCall(context, phoneNumber.text as String)
-            }
-        ) {
-            Text("Позвонить другу")
-        }
     }
 }
 
